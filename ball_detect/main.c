7a2931964b61a526c612e2f6ee7b27b202c13799
Meli <martin.melichercik@gmail.com>
2015-01-24 21:44:47 +0100

    adding USART Initialization and RX Interrupt

--- ball_detect/main.c
+++ ball_detect/main.c
@@ -1,7 +1,23 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+#define F_OSC F_CPU
 
+void setup(void) {
+	//USART INITIALIZATION
+	UCSR0A = 0;
+	UCSR0B = 0b10011000;
+	UCSR0C = 0b00000110;
+	UBRR0H = 0;
+	UBRR0L = 12;
+	sei();
+}
+
+ISR(USART_RX_vect) {
+
+}
 
 int main(void) {
-	
+	setup();
+	return 0;
 }
