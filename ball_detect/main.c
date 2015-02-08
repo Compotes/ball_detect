1ffc0744c4ee6fe9aaa2e24404a41194bef06a0e
Meli <martin.melichercik@gmail.com>
2015-02-08 18:42:53 +0100

    repair of detecting lenghts of pulses

--- ball_detect/main.c
+++ ball_detect/main.c
@@ -1,8 +1,10 @@
 #include <avr/io.h>
+#include <stdlib.h>
 #include <stdint.h> 
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include <inttypes.h>
+#include <math.h>
 #define F_OSC F_CPU
 
 #define BALL_SENSOR_1 0
@@ -10,14 +12,17 @@
 #define BALL_SENSOR_3 2
 #define ULTRASONIC_SENSOR 4
 
-volatile int32_t pulse = 0;
+volatile int32_t pulse[4];
 volatile int32_t pulses[4]; // averages of pulses
-volatile int32_t counters[4]; // counters of lenghts of pulses
 volatile int32_t helper[4]; // lenghts of pulses
+volatile int32_t result[3]; //results of ball sensor visions :)
+uint8_t changed_bits;
+uint8_t pinstate;
+uint8_t portb_history = 0xFF;
 
 void setup(void) {
 	//USART INITIALIZATION
-	//UCSR0A = 0;
+	UCSR0A = 0;
 	UCSR0B = 0b10011000;
 	UCSR0C = 0b00000110;
 	UBRR0H = 0;
@@ -30,55 +35,13 @@ void setup(void) {
 
 	//TIMER SETUP
 	TCCR1A = 0;
-	TCCR1B = 2 << CS10;
+	TCCR1B = 2;
 	TCNT1 = 0;
 
 	//TURN ON INTERRUPTS
 	sei();
 	
 	DDRD |= 1 << 3;
-}
-
-ISR(USART_RX_vect) {
-	
-}
-
-ISR(PCINT0_vect) {
-	static uint8_t portb_history = 0xFF;
-	uint8_t changed_bits;
-	uint8_t pinstate;
-
-	pinstate = PINB;
-	PORTD ^= 1 << 3;
-	changed_bits = pinstate ^ portb_history;
-	portb_history = pinstate;  
-	int ct;
-	for (ct = 0;ct < 4;ct++) {
-		if (changed_bits & (1 << ct)) {
-       		if (pinstate & (1 << ct)) {
-				helper[ct] = TCNT1H; 
-		   		if(counters[ct] < 10) { 
-					counters[ct]++; 
-				} else {
-	        		counters[ct] = 0;
-					pulses[ct] = pulses[ct]/10;
-   	     		}  			    	 	
-			} else {
-				pulse = TCNT1;
-				if(pulse < helper[ct]) {
-					pulses[ct] += pulse+2000000-helper[ct];
-					helper[ct] = 0;
-				} else {
-					pulses[ct] += pulse - helper[ct];
-				}
-			}
-		}
-	}
-    
-	if(changed_bits & (1 << ULTRASONIC_SENSOR)) {
-		
-	}
-
 }
 
 #define waitForTX() while (!(UCSR0A & 1<<UDRE0))
@@ -88,39 +51,54 @@ void sendc(uint8_t ch) {
   UDR0 = ch;
 }
 
-int main(void) {
-	uint8_t i;
-	setup();
- 	int x, j, y;
-	i = 0;
-	while (1) {
-	    x = pulses[i] / 2;
-	    y = 10000;
-		sendc(i + '0');
-        sendc(' ');
-		for(j = 0;j < 5;j++) {
-			sendc('0' + ((x / y) % 10));
-			x %= y;
-			y /= 10;
+
+
+ISR(USART_RX_vect) {
+	
+}
+
+ISR(PCINT0_vect) {
+	pinstate = PINB;
+	PORTD ^= 1 << 3;
+	changed_bits = pinstate ^ portb_history;
+	portb_history = pinstate;  
+	int ct = 0;
+	for (ct = 0;ct < 4;ct++) {
+		if (changed_bits & (1 << ct)) { 
+       		if (pinstate & (1 << ct)) { 
+				helper[ct] = TCNT1; 
+			} else {
+				pulse[ct] = TCNT1;
+				pulses[ct] = abs(pulse[ct]-helper[ct]);
+			}
 		}
+    }
+	if(changed_bits & (1 << ULTRASONIC_SENSOR)) {
+		
+	}
+
+}
+
+int main(void) {
+	uint8_t j, index = 0;
+ 	uint32_t  small;
+	setup();
+	while (1) {
+		small = 9999999;
+		result[0] = pulses[0];
+		result[1] = pulses[1];
+		result[2] = pulses[2];
+		for(j = 0;j < 3;j++) {
+			if(small > result[j]) {
+				small = result[j];
+				index = j;
+			}
+		}
+		sendc('0' + index);
 		sendc('\r');
         sendc('\n');
-		i = (i + 1) & 0x3;
+		_delay_ms(200);
 	}
 
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
