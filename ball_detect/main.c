b7e05e41463cedbee2b31fad9e5b3a00dfb4708b
Meli <martin.melichercik@gmail.com>
2015-02-13 22:40:56 +0100

    Adding communication with other device and detect its address for communication

--- ball_detect/main.c
+++ ball_detect/main.c
@@ -10,6 +10,12 @@
 #define BALL_SENSOR_1 0
 #define BALL_SENSOR_2 1
 #define BALL_SENSOR_3 2
+#define DIP_SWITCH_1 0
+#define DIP_SWITCH_2 1
+#define DIP_SWITCH_3 2 
+#define DIP_SWITCH_4 3
+#define DIP_SWITCH_5 4
+#define DIP_SWITCH_6 5
 #define F_OSC F_CPU
 #define ULTRASONIC_SENSOR 4
 #define waitForTX() while (!(UCSR0A & 1<<UDRE0))
@@ -17,10 +23,8 @@
 volatile int32_t ends_of_pulses[4];
 volatile int32_t lenghts_of_pulses[4];
 volatile int32_t starts_of_pulses[4]; 
-volatile int32_t vision_result[3];
-uint8_t changed_bits;
-uint8_t pinstate, ct;
-uint8_t portb_history = 0xFF;
+volatile int32_t vision_result[3] = {0,0,0};
+volatile uint8_t pinstate, ct, changed_bits, portb_history = 0xFF, my_address;
 
 void setup(void) {
 	//USART INITIALIZATION
@@ -32,6 +36,12 @@ void setup(void) {
 	
 	//LED REGISTER SETUP
 	DDRD |= 1 << 3;	
+
+	//DIP-SWITCH INITIALIZATION
+    DDRC = 0;
+	PORTC |= \
+		((1 << DIP_SWITCH_1) | (1 << DIP_SWITCH_2) | (1 << DIP_SWITCH_3) | \
+		 (1 << DIP_SWITCH_4) | (1 << DIP_SWITCH_5) | (1 << DIP_SWITCH_6));	
 	
 	//INTERRUPTS INIT
 	DDRB = 0;
@@ -44,6 +54,17 @@ void setup(void) {
 	TCCR1B = 2;
 	TCNT1 = 0;
 
+	//DETECT ADRESS
+	if((PINC & (1 << DIP_SWITCH_1)) == 0) {
+		my_address = 1;
+	} else if((PINC & (1 << DIP_SWITCH_2)) == 0) {
+		my_address = 2;
+	} else if((PINC & (1 << DIP_SWITCH_3)) == 0) {
+		my_address = 3;
+	} else if((PINC & (1 << DIP_SWITCH_4)) == 0) {
+		my_address = 4;
+	}
+	
 	//TURN ON INTERRUPTS
 	sei();
 }
@@ -54,10 +75,23 @@ void sendc(uint8_t ch) {
 }
 
 ISR(USART_RX_vect) {	
+	PORTD ^= 1 << 3;
+	uint32_t modulo, position;
+	uint8_t read_char = UDR0, i, j;
+	if(read_char == my_address) {
+		for(j = 2; j <= 0; j--) {	
+			modulo = vision_result[j];
+			position = 1000000;
+			for(i = 0;i < 7;i++) {
+				sendc('0' + ((modulo / position) % 10));
+				modulo %= position;
+				position /= 10;
+			}
+		}
+	} 		
 }
 
 ISR(PCINT0_vect) {
-	PORTD ^= 1 << 3;
 	pinstate = PINB;
 	changed_bits = pinstate ^ portb_history;
 	portb_history = pinstate;  
@@ -76,24 +110,11 @@ ISR(PCINT0_vect) {
 }
 
 int main(void) {
-	uint8_t j, index = 0;
- 	uint32_t small;
 	setup();
 	while (1) {
-		small = 9999999;
 		vision_result[0] = lenghts_of_pulses[0];
 		vision_result[1] = lenghts_of_pulses[1];
 		vision_result[2] = lenghts_of_pulses[2];
-		for(j = 0; j < 3; j++) {
-			if(small > vision_result[j]) {
-				small = vision_result[j];
-				index = j;
-			}
-		}
-		sendc('0' + index);
-		sendc('\r');
-        sendc('\n');
-		_delay_ms(200);
 	}
 	return 0;
 }
